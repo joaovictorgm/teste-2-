822155de44f580d0b60fb0671c9dac3085fb3445
JOAO VICTOR GABRIEL MARTINS <joao.martins8@alunos.sc.senac.br>
2026-02-19 20:11:35 -0300

    Alterando novamente

--- Teste2/src/main/java/com/example/Teste2/Teste2.java
+++ Teste2/src/main/java/com/example/Teste2/Teste2.java
@@ -2,6 +2,6 @@ package com.example.Teste2;
 
 public class Teste2 {
     public void fazendoTeste(){
-        System.out.printf("Realizando teste");
+        System.out.printf("Realizando teste novamente");
     }
 }
